361ba5feb607d7f0dd27bfdb55fe844a884a968a
aravinds90g <aravinds90g@gmail.com>
2025-08-21 13:14:43 +0530

    add searchRange

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1763,6 +1763,22 @@ public class Array {
   //   return false;
   // }
 
+  public int[] searchRange(int[] arr, int target) {
+    int[] ans = new int[2];
+
+    ans[0] = -1;
+    ans[1] = -1;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == target && ans[0] == -1) {
+        ans[0] = i;
+        ans[1] = i;
+      } else if (arr[i] == target) {
+        ans[1] = i;
+      }
+    }
+    return ans;
+  }
+
   public static void main(String[] args) {
        
   }
