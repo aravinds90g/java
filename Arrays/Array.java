0a639b3693113d0b08ed4419e45dc6a959ecd4ed
aravinds90g <aravinds90g@gmail.com>
2025-09-04 12:16:50 +0530

    add judegeCircle

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -2066,7 +2066,27 @@ public class Array {
       return (int) num * sign;
     }
 
+    public boolean judgeCircle(String moves) {
+      int count = 0;
+      int count1 = 0;
+      for (int i = 0; i < moves.length(); i++) {
+        if (moves.charAt(i) == 'U') {
+          count++;
+        } else if (moves.charAt(i) == 'D') {
+          count--;
+        } else if (moves.charAt(i) == 'L') {
+          count1++;
+        } else {
+          count1--;
+        }
+      }
 
+      if (count == 0 && count1 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
     
 
   public static void main(String[] args) {
