d77a573264e1cab4f58cbd9c045611ac0f041dea
aravinds90g <aravinds90g@gmail.com>
2025-09-15 11:34:05 +0530

    add countVowel in recursion

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -38,9 +38,33 @@ public class Recursion {
 
         return x * power(x, n-1);
     }
+    
+    public static int countVowel(String str){
+        if (str.isEmpty() || str == null) {
+            return 0;
+        }
+
+       char firstChar = Character.toLowerCase(str.charAt(0));
+
+       int count = isVowel(firstChar) ? 1 : 0;
+
+       return count + countVowel(str.substring(1));
+      
+    }
+
+    public static boolean  isVowel(char c){
+        if(c =='a' || c=='e' ||c == 'i' || c=='o' || c=='u'){
+           return true;
+        }
+        return false;
+    }
+
+    
+
 
     public static void main(String[] args) {
         // System.out.println(sumOfFirstNth(5));
-        System.out.println(power(2, 5));
+        // System.out.println(power(2, 5));
+        System.out.println(countVowel("aeiou"));
     }    
 }
